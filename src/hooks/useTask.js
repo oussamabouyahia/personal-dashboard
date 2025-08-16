160f899cee6d380e4f1c87e8054fbe0fce20730d
oussamabouyahia <oussemabenbouali.bouyahia@gmail.com>
2025-08-16 18:10:26 +0100

    user could control the task difficulty/priority

--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -1,12 +1,10 @@
-import { useState ,useMemo} from "react";
-import { mockTasks} from "../mockData"
+import { useState, useMemo } from "react";
+import { mockTasks } from "../mockData";
 
-
-export default function useTask(newTask,setNewTask){
-
-const [tasks, setTasks] = useState(mockTasks);
-const [showAddTask, setShowAddTask] = useState(false);
-const addTask = () => {
+export default function useTask(newTask, setNewTask) {
+  const [tasks, setTasks] = useState(mockTasks);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const addTask = () => {
     if (newTask.trim()) {
       const task = {
         id: Date.now(),
@@ -20,19 +18,33 @@ const addTask = () => {
       setShowAddTask(false);
     }
   };
-   const toggleTask = (id) => {
-    setTasks(prev=>
+  const handlePriority = (id, priority) => {
+    setTasks((prev) =>
+      prev.map((task) => {
+        return task.id === id ? { ...task, priority: priority } : task;
+      })
+    );
+  };
+  const toggleTask = (id) => {
+    setTasks((prev) =>
       prev.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
       )
     );
-    
   };
   const completedTasks = useMemo(
-    () => tasks.filter(t => t.completed).length,
+    () => tasks.filter((t) => t.completed).length,
     [tasks]
   );
 
-return {tasks,setTasks,showAddTask,setShowAddTask,addTask,toggleTask,completedTasks}
-
+  return {
+    tasks,
+    setTasks,
+    showAddTask,
+    setShowAddTask,
+    addTask,
+    toggleTask,
+    completedTasks,
+    handlePriority,
+  };
 }
